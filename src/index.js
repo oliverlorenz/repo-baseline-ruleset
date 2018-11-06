f9f960108275878df6a5a2fc3552e6a40505e0aa
Oliver Lorenz <mail@oliverlorenz.com>
2018-11-06 21:01:23 +0100

    save

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 const Promise = require('bluebird');
+const packageJson = require('../package.json')
 
-function RuleSet(config, pluginManager) {
+function RuleSet(pluginManager, repoPath, config) {
     function getRules() {
-        return config || [];
+        return config || packageJson['repo-baseline'] || [];
     }
     
-    function run(repoPath, options, level, callback) {
+    function run(callback, level = 1, options = {}) {
         const rules = getRules()
         return Promise.resolve(rules)
             .each((ruleSet) => {
+                console.log(`${"  ".repeat(level)} ${ruleSet.name}`)
                 return pluginManager.installFromNpm(ruleSet.name)
                     .then(() => {
                         return pluginManager.require(ruleSet.name)
                     })
                     .then((package) => {
-                        return package.run(repoPath, ruleSet.options, ++level, callback)
+                        console.log(package)
+                        const plugin = package(pluginManager, repoPath);
+                        return plugin.run(callback, ++level, ruleSet.options);
                     });
             });
     }
      
     return {
-        getRules,
         run
     }
 }
